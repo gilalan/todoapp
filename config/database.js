6c452025f40d578e760c3602a832bcaf909c8c8a
Gilliard Lopes <gilalan@gmail.com>
2017-01-14 12:24:17 -0300

    first commit todo project - set up

---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+    url : 'mongodb://admin:123456@jello.modulusmongo.net:27017/a4Tenimi'
+};
+
+//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+//mongoose.connect('admin:123456@jello.modulusmongo.net:27017/a4Tenimi');     // connect to mongoDB database on modulus.io
+//mongodb://<user>:<pass>@jello.modulusmongo.net:27017/a4Tenimi
+//mongodb://admin:123456@jello.modulusmongo.net:27017/a4Tenimi
